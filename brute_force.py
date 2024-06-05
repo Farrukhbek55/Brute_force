2d2d986a402a264132b015c7b0af5ebded9e4d0b
Farrukhbek Abdurakhimov <temursharifboyev545@gmail.com>
2024-06-05 20:12:25 +0500

    brute_force
    brute force codes for developers.Open code

---
+++ brute_force.py
@@ -0,0 +1,16 @@
+
+
+import itertools
+
+chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
+attempt = 1
+passcode = "target"
+for password_length in range(1, 9):
+    for guess in itertools.product(chars, repeat=password_length):
+        brute_passcode = ''.join(guess)
+        print(f"Attempt {attempt}: {brute_passcode}")
+        attempt += 1
+        
+        if brute_passcode == passcode:
+            print("Parol topildi!")
+            break
